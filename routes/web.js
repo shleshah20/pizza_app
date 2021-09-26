11368f71bc6e5eaadea31d1711d40d48b4bb9d80
shlesha.shah <shlesha.shah@diamnow.com>
2021-09-26 18:06:52 +0530

    creating controller and connect them with routes

--- routes/web.js
+++ routes/web.js
@@ -1,16 +1,15 @@
-function initRoutes(app){
-    app.get('/',(req,res) => {
-        res.render('home')
-    })
-    app.get('/cart',(req,res)=>{
-        res.render('customer/cart')
-    })
-    app.get('/login',(req,res)=>{
-        res.render('auth/login')
-    })
-    app.get('/register',(req,res)=>{
-        res.render('auth/register')
-    })
+const authController = require('../app/http/controllers/authController')
+const cartController = require('../app/http/controllers/customers/cartController')
+const homeController = require('../app/http/controllers/homeController')
+
+function initRoutes(app){    
+    app.get('/',homeController().index)
+   
+    app.get('/cart',cartController().index)
+
+    app.get('/login',authController().login)
+
+    app.get('/register',authController().register)
 }
 
 module.exports = initRoutes
