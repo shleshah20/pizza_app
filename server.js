a1644b2ae512c1769352e42cbc782e4f95655c86
shlesha.shah <shlesha.shah@diamnow.com>
2021-10-03 16:48:38 +0500

    connect mongoose and fetch all pizza data dynamically

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+// const session = require('express-session')
+//database connection
+const url = 'mongodb://localhost:27017/pizza';
+
+mongoose.connect(url,{ 
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(() => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log(err);
+});
+
+//session config
+
 //assets 
 app.use(express.static('public'))
 
@@ -13,7 +29,7 @@ app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
-require('./routes/web.js')(app)
+ require('./routes/web.js')(app)
 
 
 app.listen(PORT,() => {
