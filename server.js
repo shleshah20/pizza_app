4b0c05a31e30dde5f3c24772958c93d9337eb2bb
shlesha.shah <shlesha.shah@diamnow.com>
2021-10-19 12:18:07 +0500

    add  authentication using passport plugin

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')
-
+const passport = require('passport')
 //database connection
 const url = 'mongodb://localhost:27017/pizza';
 
@@ -38,15 +38,23 @@ app.use(session({
     // cookie:{maxAge:1000*15}
 }))
 
+//passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 
 //assets 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 //Global middleware
 app.use((req,res,next)=>{
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
